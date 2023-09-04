86d2bbb26069aac4bc0eacdd2c8577e8b4500dda
Krishnaaa-29 <acctocode29@gmail.com>
2023-09-05 00:36:26 +0530

    home-layout

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,4 +1,26 @@
+import HTMLReactParser from "html-react-parser";
+import { useParams } from "react-router-dom";
+import millify from "millify";
+import { Col, Row, Typography, Select } from "antd";
+import {
+  MoneyCollectOutlined,
+  DollarCircleOutlined,
+  FundOutlined,
+  ExclamationCircleOutlined,
+  StopOutlined,
+  TrophyOutlined,
+  CheckOutlined,
+  NumberOutlined,
+  ThunderboltOutlined,
+} from "@ant-design/icons";
+import { useState } from "react";
+
+const { Title, Text } = Typography;
+const { Option } = Select;
+
 const CryptoDetails = () => {
+  const { coinId } = useParams();
+  const [timePeriod, setTimePeriod] = useState();
   return <div>CryptoDetails</div>;
 };
 export default CryptoDetails;
